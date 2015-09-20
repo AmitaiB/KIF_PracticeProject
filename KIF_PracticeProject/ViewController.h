965ce06317a9a088503ab8634766813f83bfd010
AmitaiB <blickstein@gmail.com>
2015-09-20 14:52:49 -0400

    Initial commit

---
+++ KIF_PracticeProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KIF_PracticeProject
+//
+//  Created by Amitai Blickstein on 9/20/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KIF_PracticeProjectTests/KIF_PracticeProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  KIF_PracticeProjectTests.m
+//  KIF_PracticeProjectTests
+//
+//  Created by Amitai Blickstein on 9/20/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KIF_PracticeProjectTests : XCTestCase
+
+@end
+
+@implementation KIF_PracticeProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
